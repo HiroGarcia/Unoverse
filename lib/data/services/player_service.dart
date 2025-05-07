e9a9053b734da4431923bba9c43886437cc453d3
HiroGarcia <Gui.garcia123654@hotmail.com>
2025-05-07 15:05:31 -0300

    feature: created read players function

--- lib/data/services/player_service.dart
+++ lib/data/services/player_service.dart
@@ -5,6 +5,22 @@ import '../../domain/entity/player_entity.dart';
 class PlayerService {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
+  Future<List<Player>> readPlayer({required String groupId}) async {
+    List<Player> temp = [];
+
+    QuerySnapshot<Map<String, dynamic>> snapshot =
+        await firestore
+            .collection("groups")
+            .doc(groupId)
+            .collection("players")
+            .get();
+
+    for (var doc in snapshot.docs) {
+      temp.add(Player.fromMap(doc.data()));
+    }
+    return temp;
+  }
+
   Future<void> addPlayer({
     required Player player,
     required String groupId,
--- lib/presentation/pages/group/group_page.dart
+++ lib/presentation/pages/group/group_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:unoverse/domain/entity/enum_type.dart';
 
@@ -21,6 +22,7 @@ class _GroupScreenState extends State<GroupPage> {
 
   @override
   void initState() {
+    refresh();
     super.initState();
   }
 
@@ -38,7 +40,60 @@ class _GroupScreenState extends State<GroupPage> {
         },
         child: Icon(Icons.add),
       ),
-      body: Container(),
+      body:
+          (listPlayer.isEmpty)
+              ? const Center(
+                child: Text(
+                  "Nenhuma lista ainda.\nVamos criar a primeira?",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18),
+                ),
+              )
+              : Container(
+                color: Colors.blue[50],
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 200,
+                  ),
+                  itemCount: listPlayer.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      color: Colors.amber,
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              listPlayer[index].name,
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              'Pontuação total: ${listPlayer[index].totalScore}',
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
     );
+  }
+
+  refresh() async {
+    try {
+      listPlayer = await playerService.readPlayer(
+        groupId: widget.group.groupId,
+      );
+      setState(() {});
+    } on FirebaseException catch (e) {
+      print('Erro: ${e.code}');
+    } catch (e) {
+      print('Erro: $e');
+    }
   }
 }
